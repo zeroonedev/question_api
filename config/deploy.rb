d5bb0258887a08cf5947ef5020ce36e6f31d495c
Steven Holloway <impurist@gmail.com>
2013-07-23 11:10:10 +1000

    deploy to 'deploy' user account home directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,36 +12,13 @@ set :group, "www-data"
 
 set :scm, :git
 set :repository, "git@github.com:zeroonedev/#{application}.git"
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/deploy/#{application}"
 set :deploy_via, :remote_cache
 set :rails_env, 'production'
 set :use_sudo, false
 set :normalize_asset_timestamps, false
-set :git_enable_submodules, 1
-
-# set :grunt_tasks, 'build'
-# set :grunt_options, '--gruntfile Gruntfile.js'
-
-set :npm_options, ''
 
 default_run_options[:pty] = true
-
-  namespace :grunt_sub do
-    desc 'Runs the Grunt tasks or the default task if none are specified in grunt_tasks.'
-    task :default, :roles => :app, :except => { :no_release => true } do
-      tasks = Array(grunt_tasks)
-      tasks.each do |task|
-        try_sudo "cd #{latest_release}/question_app && grunt #{grunt_options} #{task}"
-      end
-    end
-  end
-
-namespace :npm_sub do
-    desc 'Runs npm install.'
-    task :install, :roles => :app, :except => { :no_release => true } do
-      try_sudo "cd #{latest_release}/question_app && #{npm_path} #{npm_options} install"
-    end
-  end
 
 namespace :deploy do
 
@@ -61,7 +38,6 @@ namespace :deploy do
   task :restart do
     sudo "#{sudo} kill -s SIGUSR2 $(cat /var/run/puma.pid)"
   end
-
   task :uname do
     run "uname -a"
   end
@@ -70,27 +46,7 @@ namespace :deploy do
     run "ruby --v"
   end
 
-  # desc "Refresh shared node_modules symlink to current node_modules"
-  # task :refresh_symlink do
-  #   run "rm -rf #{current_path}/question_app/node_modules && ln -s #{shared_path}/node_modules #{current_path}/question_app/node_modules"
-  # end
- 
-  # desc "Install node modules non-globally"
-  # task :npm_install do
-  #   run "cd #{current_path}/question_app && npm install"
-  # end
 
-  desc "Install bower modules non-globally"
-  task :bower_install do
-    run "cd #{current_path}/question_app && bower install"
-  end
+  before "deploy:finalize_update", "deploy:migrate"
 
-  task :install_grunt do
-    run "cd #{current_path}/question_app; which grunt"
-  end
-
-
-  before "deploy:finalize_update", "deploy:migrate", "npm_sub:install", "deploy:bower_install"
-  # after "deploy:npm_install", "deploy:install_grunt"
-  # after 'deploy:finalize_update', 'grunt_sub'
 end
