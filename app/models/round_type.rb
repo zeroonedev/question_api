8f906d413901d4d178dd277f1b328324f0becd50
Lyndon Maydwell <lyndon@sordina.net>
2013-09-17 10:54:35 +1000

    Adding reverse association fk for rails-admin

--- app/models/round_type.rb
+++ app/models/round_type.rb
@@ -4,6 +4,7 @@ class RoundType < ActiveRecord::Base
                   :number_of_questions,
                   :number_of_spares
 
-  has_many :rounds
+  has_many :rounds, :foreign_key => :type_id
+
   belongs_to :question_type
 end
